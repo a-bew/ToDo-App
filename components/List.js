1c35f7f2dc47b7e4bba48e5d00657df686b14df0
Adetola Bewaji <adetola013@gmail.com>
2019-03-20 23:42:21 +0100

    updated List.js by changing List func to react component and introduce a new deleteItem(item){this.props.OnDelete(item);} method to handle delete with an onClick event

--- components/List.js
+++ components/List.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const List = props => (
-  <ul>
-    {
-      props.items.map((item, index) => <li key={index}>{item}</li>)
+class List extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+
     }
-  </ul>
-);
+  }
 
+  deleteItem = (item) => {
+    console.log(item)
+
+    this.props.onDelete(item);
+  }
+
+  render() {
+      return (
+    <ul>
+      {
+        this.props.items.map((item, index) => <li key={index}>{item.term}<a href='#' onClick= {this.deleteItem.bind(this, item.id) }> X </a></li>)
+      }
+    </ul>
+    );
+  }
+}
 export default List;
