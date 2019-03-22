df2b9c5fdc74f1468ad01c85d5ea1e118c8575da
Adetola Bewaji <adetola013@gmail.com>
2019-03-22 13:06:58 +0100

    updated pages/index.js by changing div tag to header and sytling

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,55 @@
 import Layout from '../components/Layout';
-
+import ToDo from '../components/ToDo';
 const Index = () => (
   <Layout>
+
     <div>
-      <h1>To-Do App</h1>
+     <div className="width card">
+     <header class="card-body">
+         <h1 class="card-title">To Do List</h1>
+         <p class="card-text">A Quick to-do app with task completion color checker </p>
+         <h6>Created by <a href="#">Adetola Bewaji</a></h6><span>Date: 22/03/2019</span>
+     </header>
+     <ToDo />
+      </div>
+      <style jsx>{`
+        h6{
+          left: 5px;
+          float:left;
+          top: 10px;
+        }
+
+        span{
+          right: 5px;
+          float:right;
+          cursor:pointer;
+          top: 10px;
+        }
+
+        .width{
+          margin: auto;
+          width: 50%;
+        }
+
+          #img{
+            background-image: yellow;
+            width: 100%;
+            heigth: 20vh;
+          }
+
+          h1, p{
+            color: purple;
+            font-family: Arial black;
+            text-align:center;
+          }
+          header{
+            background-image: url("/static/img/task.jpg");
+          }
+
+      `}</style>
     </div>
+
+
   </Layout>
 )
 export default Index;
