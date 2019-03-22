8bf88e837ad3974432e71a53eb3e11594deb5d9a
Adetola Bewaji <adetola013@gmail.com>
2019-03-22 13:02:56 +0100

    updated components/ToDo.js by adding checkBox method to manage and update for task checker

--- components/ToDo.js
+++ components/ToDo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import List from './List';
 import uuid from 'uuid';
+import PropTypes from 'prop-types';
 
 let  count = 0
 class ToDo extends Component {
@@ -26,27 +27,73 @@ class ToDo extends Component {
     this.setState({id: this.state.items.length, term: event.target.value });
   }
 
+  checkBox = (id, item, isChecked, done) => {
+    let items = this.state.items;
+    items.forEach(object => {
+
+      if (object.id == id){
+
+        console.log(id)
+        object.getname = item;
+        object.isChecked = isChecked;
+        object.setColor = done
+      }
+    });
+    console.log(items)
+    this.setState({items:items})
+ }
   onSubmit = (event) => {
     event.preventDefault();
+    if (!this.state.term) return
     this.setState({
       term: '',
-      items: [...this.state.items, { id: uuid.v4(), term: this.state.term }]
-
-
+      items: [...this.state.items, { id: uuid.v4(), term: this.state.term, getname : "", isChecked: "", setColor: ""}]
     });
   }
+
 
   render() {
     return (
       <div>
-        <form className="ToDO" onSubmit={this.onSubmit}>
-          <input value={this.state.term} onChange={this.onChange} />
-          <button>Submit</button>
-        </form>
-        <List items={this.state.items} onDelete={this.onDelete} />
-        </div>
+        <div className="width marginbuttom">
+
+          <form className="ToDO" onSubmit={this.onSubmit}>
+            <input placeholder="Add a new to-do item" className="form-control" value={this.state.term} onChange={this.onChange} />
+            <button className="btn btn-primary btn-block">Submit Task</button>
+          </form>
+       </div>
+
+      <div className="tasks">
+         <List  items={this.state.items} onDelete={this.onDelete} checkBox = {this.checkBox} />
+       </div>
+
+
+        <style jsx>{`
+        .task{
+            margin-top: 20px 0;
+          }
+          .marginbuttom{
+            margin: 10px 0;
+          }
+
+
+
+          ul li {
+              position: relative;
+              padding: 5px;
+              margin: 5px;
+              background-color: #ededed;
+              border: 2px inset #ededed;
+          }
+        `}</style>
+
+      </div>
     );
   }
+}
+
+ToDo.propTypes = {
+  checkBox: PropTypes.func.isRequired,
 }
 
 export default ToDo;
