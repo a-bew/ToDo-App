6c433bd6e32cdf979455b5a3538750541a0a66c6
Adetola Bewaji <adetola013@gmail.com>
2019-03-25 09:44:48 +0100

    added a static method getInitialProps, to index.js page

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,80 @@
+import React, { Component } from 'react';
 import Layout from '../components/Layout';
 import ToDo from '../components/ToDo';
-const Index = () => (
-  <Layout>
+import fetch from 'isomorphic-unfetch';
 
-    <div class = "row">
-     <div className="width card">
-     <header className="card-body">
-       <h1 className="card-title">To Do List</h1>
-       <p className="card-text">A Quick to-do app with task completion color checker </p>
-       <h6>Created by <a href="#">Adetola Bewaji</a></h6><span>Date: 22/03/2019</span>
-     </header>
-     <ToDo />
-      </div>
-      <style jsx>{`
-        h6{
-          left: 5px;
-          float:left;
-        }
+class Index extends Component{
 
-        span{
-          right: 5px;
-          float:right;
-          cursor:pointer;
-        }
+  static getInitialProps = async function(){
+      const res = await fetch("http://localhost:3000/api/items");
+      const data = await res.json();
+      return { items: data.items }
+  };
 
-          #img{
-            background-image: yellow;
-            width: 100%;
-            heigth: 20vh;
+  render(){
+    return (
+    <Layout>
+
+      <div className = "row">
+       <div className="width card">
+         <header className="card-body">
+           <h1 className="card-title">To Do List</h1>
+           <p className="card-text">A Quick to-do app with task completion color checker </p>
+           <h6>Created by <a href="#">Adetola Bewaji</a></h6><span>Date: 22/03/2019</span>
+         </header>
+         <ToDo items={ this.props.items } />
+        </div>
+        <style jsx>{`
+          h6{
+            left: 5px;
+            float:left;
           }
 
-          h1, p{
-            color: purple;
-            font-family: Arial black;
-            text-align:center;
+          span{
+            right: 5px;
+            float:right;
+            cursor:pointer;
           }
 
-          h1{
-            font-size: 5em;
-          }
-          p{
-            font-size: 1.5em;
-          }
-          header{
-            background-image: url("/static/img/task.jpg");
-            background-repeat:  no-repeat;
-            background-size: cover;
+            #img{
+              background-image: yellow;
+              width: 100%;
+              heigth: 20vh;
+            }
 
-          }
+            h1, p{
+              color: purple;
+              font-family: Arial black;
+              text-align:center;
+            }
 
-          .width{
-            margin: auto;
-            width: 80%;
-          }
+            header{
+              background-image: url("/static/img/task.jpg");
+              background-repeat:  no-repeat;
+              background-size: cover;
 
-          @media (min-width: 767px){
+            }
+
             .width{
               margin: auto;
-              width: 50%;
+              width: 80%;
             }
-          }
+
+            @media (min-width: 767px){
+              .width{
+                margin: auto;
+                width: 50%;
+              }
+            }
 
 
-      `}</style>
-    </div>
+        `}</style>
+      </div>
 
 
-  </Layout>
-)
+    </Layout>
+    )
+  }
+}
+
 export default Index;
